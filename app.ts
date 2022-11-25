4ac003046eac3b906dbe5894390431411247f688
yufei.zhuang <yufei.zhuang@accenture.com>
2022-11-25 16:00:01 +0800

    引入ORM框架 prisma

---
+++ app.ts
@@ -0,0 +1,39 @@
+import express from "express";
+import path from "path";
+import createError from "http-errors";
+import cookieParser from "cookie-parser";
+import logger from "morgan";
+import loginRouter from "./src/login/controller/login.controller";
+
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use('/client', express.static(path.join(__dirname, 'public/client/angular-hero')));
+
+app.use('/login', loginRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
